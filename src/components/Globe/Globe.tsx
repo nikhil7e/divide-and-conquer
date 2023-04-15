5b481677663dfac0fba45d4977ed42debd2bb0c0
Nikhil Kumar <nikhil7e@gmail.com>
2023-04-15 22:18:42 +0000

    Add basic ui

--- src/components/Globe/Globe.tsx
+++ src/components/Globe/Globe.tsx
@@ -1,23 +1,11 @@
-// import Globe from 'react-globe.gl';
-
-// export function GlobeComponent() {
-//   return (
-//     <div style={{ width: '100%', height: '100vh' }}>
-//       <Globe
-//         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-//         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-//         width={window.innerWidth}
-//         height={window.innerHeight}
-//       />
-//     </div>
-//   );
-// }
-
-import { useEffect, useState } from 'react';
-import Globe from 'react-globe.gl';
+import { useEffect, useRef, useState } from 'react';
+import Globe, { GlobeMethods } from 'react-globe.gl';
+import './Globe.css';
 
 export const GlobeComponent = () => {
   const [countries, setCountries] = useState(null);
+  const [gameState, setGameState] = useState(null);
+  const globeRef = useRef<GlobeMethods>(null);
 
   useEffect(() => {
     // load data
@@ -25,39 +13,93 @@ export const GlobeComponent = () => {
       const res = await fetch('/ne_10m_admin_0_countries.json');
       const x = await res.json();
       setCountries(x);
-      console.log(x);
     }
 
     getData();
+
+    // fetch initial game state
+    async function getGameState() {
+      const res = await fetch('http://localhost:4000/gamestate');
+      const data = await res.json();
+      setGameState(data);
+    }
+
+    getGameState();
   }, []);
 
   const handleCountryClick = (country) => {
     console.log(country);
-    // Do something with the selected country data
+    // get country stats from gameState object
+    const countryStats = gameState[country.properties.ADMIN];
+    console.log(countryStats);
+    // display stats in tooltip or modal
+  };
+
+  const hexPolygonLabel = ({ properties: d }) => {
+    console.log(gameState);
+    console.log(d);
+    const countryStats = gameState.countries.find((c) => c.name === d.ADMIN);
+    console.log(countryStats);
+    return `
+      <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+      Population: <i>${d.POP_EST}</i> <br />
+      Resources: <i>${countryStats.resources}</i>
+    `;
   };
 
   return (
     <>
-      {!countries && <h1>Loading...</h1>}
-      {countries && (
-        <Globe
-          globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
-          hexPolygonsData={countries.features}
-          hexPolygonResolution={3}
-          hexPolygonMargin={0.3}
-          hexPolygonColor={() =>
-            `#${Math.round(Math.random() * Math.pow(2, 24))
-              .toString(16)
-              .padStart(6, '0')}`
-          }
-          hexPolygonAltitude={0.01}
-          // @ts-ignore
-          hexPolygonLabel={({ properties: d }) => `
-                <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-                Population: <i>${d.POP_EST}</i>
-              `}
-          onHexPolygonClick={handleCountryClick}
-        />
+      {(!countries || !gameState) && <h1>Loading...</h1>}
+      {countries && gameState && (
+        <>
+          <div className="overlay" style={{ pointerEvents: 'none' }}>
+            <div className="header">
+              <h1>World Domination</h1>
+              <p>Turn {gameState.turn}</p>
+            </div>
+            <div className="stats">
+              <h2>Your Country</h2>
+              <ul>
+                <li>Country: x</li>
+                <li>Population: x</li>
+                <li>Resources: x</li>
+                {/* <li>Army: {playerCountry.armySize}</li> */}
+              </ul>
+            </div>
+            <div className="actions">
+              <h2>Actions</h2>
+              <ul>
+                <li>
+                  <button>End Turn</button>
+                </li>
+                <li>
+                  <button>Build Army</button>
+                </li>
+                <li>
+                  <button>Attack</button>
+                </li>
+                <li>
+                  <button>Diplomacy</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Globe
+            ref={globeRef}
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+            hexPolygonsData={countries.features}
+            hexPolygonResolution={3}
+            hexPolygonMargin={0.3}
+            hexPolygonColor={() =>
+              `#${Math.round(Math.random() * Math.pow(2, 24))
+                .toString(16)
+                .padStart(6, '0')}`
+            }
+            hexPolygonAltitude={0.01}
+            hexPolygonLabel={hexPolygonLabel}
+            onHexPolygonClick={handleCountryClick}
+          />
+        </>
       )}
     </>
   );
