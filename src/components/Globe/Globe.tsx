340179e7311d13a9363bcb9002d1e3177dd5674d
Nikhil Kumar <nikhil7e@gmail.com>
2023-04-16 04:24:50 +0000

    Add highlight on hover

--- src/components/Globe/Globe.tsx
+++ src/components/Globe/Globe.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef, useState } from 'react';
+import * as d3 from 'd3';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import Globe, { GlobeMethods } from 'react-globe.gl';
 import './Globe.css';
 
 export const GlobeComponent = () => {
-  const [countries, setCountries] = useState(null);
+  const [countries, setCountries] = useState({ features: [] });
   const [gameState, setGameState] = useState(null);
   const globeRef = useRef<GlobeMethods>(null);
+  const [hoverD, setHoverD] = useState(null);
 
   useEffect(() => {
     // load data
@@ -27,24 +29,53 @@ export const GlobeComponent = () => {
     getGameState();
   }, []);
 
+  const showCountry = () => {
+    // find the selected country in the game state
+    const selectedCountry = gameState.playerCountry.name;
+    // find the matching feature for the selected country in the countries data
+    const selectedFeature = countries.features.find((feature) => feature.properties.ADMIN === selectedCountry);
+    // fly to the selected country
+    // globeRef.current.toGlobeCoords(
+    //   selectedFeature.properties.coordinates[0],
+    //   selectedFeature.properties.coordinates[1]
+    // );
+    console.log(selectedFeature.geometry.coordinates[0]);
+    globeRef.current?.pointOfView(
+      {
+        lat: selectedFeature.geometry.coordinates[0][0][0][1],
+        lng: selectedFeature.geometry.coordinates[0][0][0][0],
+        altitude: 1,
+      },
+      500
+    );
+  };
+
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = (feat) => feat.properties.GDP_MD_EST;
+
+  const maxVal = useMemo(() => Math.max(...countries.features.map(getVal)), [countries]);
+  colorScale.domain([0, maxVal]);
+
   const handleCountryClick = (country) => {
     console.log(country);
     // get country stats from gameState object
-    const countryStats = gameState[country.properties.ADMIN];
+    const countryStats = gameState.countries.find((c) => c.name === country.properties.ADMIN);
     console.log(countryStats);
-    // display stats in tooltip or modal
   };
 
   const hexPolygonLabel = ({ properties: d }) => {
-    console.log(gameState);
-    console.log(d);
     const countryStats = gameState.countries.find((c) => c.name === d.ADMIN);
-    console.log(countryStats);
     return `
       <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
       Population: <i>${d.POP_EST}</i> <br />
       Resources: <i>${countryStats.resources}</i>
     `;
+  };
+
+  const handleHexPolygonHover = (hexPolygon) => {
+    setHoverD(hexPolygon);
   };
 
   return (
@@ -57,12 +88,10 @@ export const GlobeComponent = () => {
               <h1>World Domination</h1>
               <p>Turn {gameState.turn}</p>
             </div>
-            <div className="stats">
-              <h2>Your Country</h2>
+            <div className="stats" onClick={showCountry}>
+              <h2>{gameState.playerCountry.name}</h2>
               <ul>
-                <li>Country: x</li>
-                <li>Population: x</li>
-                <li>Resources: x</li>
+                <li>Resources: {gameState.playerCountry.resources}</li>
                 {/* <li>Army: {playerCountry.armySize}</li> */}
               </ul>
             </div>
@@ -87,16 +116,17 @@ export const GlobeComponent = () => {
           <Globe
             ref={globeRef}
             globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
-            hexPolygonsData={countries.features}
-            hexPolygonResolution={3}
-            hexPolygonMargin={0.3}
-            hexPolygonColor={() =>
-              `#${Math.round(Math.random() * Math.pow(2, 24))
-                .toString(16)
-                .padStart(6, '0')}`
-            }
-            hexPolygonAltitude={0.01}
+            hexPolygonsData={countries.features.filter((d) => d.properties.ISO_A2 !== 'AQ')}
+            lineHoverPrecision={0}
+            hexPolygonResolution={4}
+            hexSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+            hexPolygonMargin={0}
+            hexPolygonAltitude={0.1}
+            hexPolygonColor={(d) => (d === hoverD ? 'steelblue' : colorScale(getVal(d)))}
             hexPolygonLabel={hexPolygonLabel}
+            onHexPolygonHover={handleHexPolygonHover}
+            hexPolygonsTransitionDuration={300}
+            hexPolygonCurvatureResolution={0}
             onHexPolygonClick={handleCountryClick}
           />
         </>
@@ -104,3 +134,5 @@ export const GlobeComponent = () => {
     </>
   );
 };
+
+export default GlobeComponent;
